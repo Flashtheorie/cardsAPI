fa2884e2be861b5abf88abd6ea35199e300c88bd
Flashtheorie <antoinepascualmail@gmail.com>
2022-07-02 19:58:38 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'card';
+  showDrawBtn = false;
+  showCard = false;
+  cards : any = [];
+  cardsoutside: any = [];
+  deck: string | undefined;
+  url = 'https://deckofcardsapi.com/api/deck/';
+  constructor(private http: HttpClient) {
+    
+  }
+  public  shuffle(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    this.showDrawBtn = true;
+
+  
+
+  this.http
+      .get(this.url + '/new/shuffle')
+      .subscribe((res: any) => {
+        this.deck = res['deck_id'];
+        console.log(this.deck);
+      });
+
+    }
+
+    public async getCard(){
+      this.showCard = true;
+
+
+      this.http
+      .get(this.url + '/' + this.deck + '/draw/?count=1')
+      .subscribe((res: any) => {
+         this.cards = res['cards'][0];
+         this.cardsoutside = res;
+         console.log(res['deck_id']);
+
+      });
+
+    }
+
 }
+
+
+
